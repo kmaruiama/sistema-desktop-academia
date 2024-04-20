26f788399c0e75d28764d5dc27a4a44be89c66df
kmaruiama <144010667+kmaruiama@users.noreply.github.com>
2024-04-20 06:29:55 -0300

    Delete checaConcordancia.java

--- checaConcordancia.java
+++
@@ -1,9 +0,0 @@
-package isso;
-
-
-/* Essa classe serve simplesmente para checar a concordancia entre o input
-oferecido pelo usuário e os campos do servidor, impede o usuário criar/
-atualizar um campo para nulo, escrever letras em informações puramente
-númericas e vice-versa */
-public class checaConcordancia {
-}
