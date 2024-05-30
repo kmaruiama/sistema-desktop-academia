0822a7824e2420a8ec34e118652e6f743800a181
kmaruiama <144010667+kmaruiama@users.noreply.github.com>
2024-05-30 12:30:26 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,15 @@
 package paradigmasTrabalhoUm;
 
+import paradigmasTrabalhoUm.GUI.MenuPrincipal;
 import java.sql.Connection;
+import paradigmasTrabalhoUm.Database.DatabaseMetodos;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main
 {
     public static void main(String[] args) {
-        DatabaseMetodos novaDatabase = new DatabaseMetodos();
-        Connection conexao = novaDatabase.conectaDb("paradigmas_database", "postgres", "admin");
-        novaDatabase.createTableCliente(conexao);
-        novaDatabase.createListaTreino(conexao);
+        Connection conexao = DatabaseMetodos.conectaDb();
+        DatabaseMetodos.createTableCliente(conexao);
+        DatabaseMetodos.createListaTreino(conexao);
         MenuPrincipal menuPrincipal = new MenuPrincipal();
     }
 }
